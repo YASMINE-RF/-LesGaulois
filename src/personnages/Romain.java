cad5662fb267c2054661fd16935d488d2c384bb4
YASMINE-RF <yasmine.rafai04@gmail.com>
2025-10-20 16:27:59 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,8 +1,11 @@
 package personnages;
+import objets.Equipement;
 
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -47,9 +50,45 @@ public class Romain {
 	 
 	 public static void main(String[] args) {
 	        Romain minus = new Romain("Minus", 6);
+            System.out.println(Equipement.CASQUE);
+	        System.out.println(Equipement.BOUCLIER);
+	        minus.sEquiper(Equipement.CASQUE);
+	        minus.sEquiper(Equipement.CASQUE);
+	         minus.sEquiper(Equipement.BOUCLIER);
+	        minus.sEquiper(Equipement.CASQUE);
+	        
 	    }
+	 
+	 
+	 
+	 public void sEquiper(Equipement equipement) {
+		    switch (nbEquipement) {
+		        case 2:
+		            System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+		            break;
+
+		        case 1:
+		            if (equipements[0] == equipement) {
+		                System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+		            } else {
+		                ajouterEquipement(equipement);
+		            }
+		            break;
+
+		        case 0:
+		            ajouterEquipement(equipement);
+		            break;
+		    }
+		}
+
+		private void ajouterEquipement(Equipement equipement) {
+		    equipements[nbEquipement] = equipement;
+		    nbEquipement++;
+		    System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+		}
 	
 	
+	 
 	
 	
 	
