202e36b106f3574442bbac0d30635f98bf6f24e0
YASMINE-RF <yasmine.rafai04@gmail.com>
2025-10-20 15:49:20 +0200

    invariant pre post condition

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified() : "Invariant non respecté à la fin du constructeur !";
 	}
 
 	public String getNom() {
@@ -22,11 +23,34 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert forceCoup > 0 : "Précondition non respectée : la force du coup doit être positive !";
+		int ancienneForce = force;
 		force -= forceCoup;
 		if (force < 1) {
 			parler("Jabondone...");
 		} else {
 			parler("Aie");
 		}
+		
+		 
+		 assert force <= ancienneForce : "Postcondition non respectée : la force n'a pas diminué !";
+		 assert isInvariantVerified() : "Invariant non respecté à la fin de recevoirCoup() !";
 	}
+	
+	
+	
+	
+	 private boolean isInvariantVerified() {
+	        return force >= 0;
+	    }
+	
+	 
+	 public static void main(String[] args) {
+	        Romain minus = new Romain("Minus", 6);
+	    }
+	
+	
+	
+	
+	
 }
